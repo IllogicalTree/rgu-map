7c790d4b022f939cdf9a9251e9cf4a00214e8bbb
Jordan <jordan@dev4.com>
2023-02-26 06:48:05 +0000

    highlight works!!
    Co-authored-by: e-nara <e-nara@users.noreply.github.com>
    Co-authored-by: Pailinsie <Pailinsie@users.noreply.github.com>

--- 3d-test.js
+++ 3d-test.js
@@ -1,6 +1,5 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.130.0";
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.130.0/examples/jsm/controls/OrbitControls.js';
-import { EventControls } from './event-controls.js';
 import { SVGLoader } from 'https://cdn.skypack.dev/three@0.130.0/examples/jsm/loaders/SVGLoader.js';
 var dragObjects = [];
 const fillMaterial = new THREE.MeshBasicMaterial({ color: "blue", side: THREE.DoubleSide, depthWrite: false, opacity: 0 });
@@ -21,51 +20,23 @@ const colors = new Map([
   ["cls-6", "#f9b233"],
   ["cls-7", "#be1622"]
 ]);
-/*
 
+const roomPositions = new Map();
 
-.cls-1,
-        .cls-3 {
-          fill: #662483;
-        }
-
-        .cls-1 {
-          opacity: 0.55;
-        }
-
-        .cls-2 {
-          fill: #d5cbe2;
-        }
-
-        .cls-4 {
-          fill: #b02f7c;
-        }
-
-        .cls-5 {
-          fill: #3a53a0;
-        }
-
-        .cls-6 {
-          fill: #f9b233;
-        }
-
-        .cls-7 {
-          fill: #be1622;
-        }
-*/
 const renderSVG = (svg) => {
   const loader = new SVGLoader();
   const svgData = loader.parse(svg);
   const svgGroup = new THREE.Group();
   const updateMap = [];
+  console.log(svgData)
  
   svgGroup.scale.y *= -1;
   svgData.paths.forEach((path) => {
     const shapeColorCode = path.userData.node.classList[0];
+    const shapeRoomCode = path.userData.node.classList[1] ?? "0";
     const shapes = SVGLoader.createShapes(path);
 
     shapes.forEach((shape) => {
-      //console.log(shape.uuid)
       const meshGeometry = new THREE.ExtrudeBufferGeometry(shape, {
         depth: 20,
         bevelEnabled: false,
@@ -73,7 +44,9 @@ const renderSVG = (svg) => {
       const linesGeometry = new THREE.EdgesGeometry(meshGeometry);
       let mesh = new THREE.Mesh(meshGeometry, new THREE.MeshBasicMaterial({ color: colors.get(shapeColorCode), side: THREE.DoubleSide, depthWrite: false }));
       let rand = Math.random(0,1)
-      //console.log(rand)
+      if (shapeRoomCode != 0) {
+      roomPositions.set(shapeRoomCode, shape.currentPoint)
+      }
       
       let lines = new THREE.LineSegments(linesGeometry, stokeMaterial);
 
@@ -105,11 +78,15 @@ const renderSVG = (svg) => {
 
   return {
     object: svgGroup,
-    highlight(pos) {
+    highlight(roomNo) {
+      let pos = roomPositions.get(roomNo)
+      if (!pos) {
+        console.log("no position for roomno")
+      }
       //console.log(updateMap)
-      console.log(updateMap)
+      //console.log(updateMap)
       let z = updateMap.filter(x => (x.shape.currentPoint.x === pos.x && x.shape.currentPoint.y === pos.y))
-      console.log(z)
+      //console.log(z)
       //console.log(updateMap)
       z.forEach((updateDetails) => {
         //console.log(updateDetails.shape.uuid)
@@ -148,23 +125,6 @@ const setupScene = (container) => {
     0.01,
     1e5
   );
-
-  var eventControl = new EventControls( [ ... dragObjects ], camera, renderer.domElement );
-
-    eventControl.attachEvent( 'mouseOver', function () {
-
-      container.style.cursor = 'pointer';
-      this.event.object.material = selMaterial;
-      console.log( 'the key at number ' + this.event.item + ' is select' );
-
-    });
-          
-    eventControl.attachEvent( 'mouseOut', function () {
-
-      container.style.cursor = 'auto';
-      this.event.object.material = autoMaterial;
-
-    });  
 
   const ambientLight = new THREE.AmbientLight("#888888");
   const pointLight = new THREE.PointLight("#ffffff", 2, 800);
@@ -246,169 +206,169 @@ const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1563.59 604.1"
 </g>
 <g id="N309">
   <path
-    class="cls-3"
+    class="cls-3 N309"
     d="M503.31,274.47c29.05,40.36,63.14,80.71,92.18,121.07,22-15.59,39-31.18,61-46.78L544.59,212.1Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N310">
   <path
-    class="cls-3"
+    class="cls-3 N310"
     d="M609.48,391.34l62.67,76.43,47.09-37L656,355.26Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="Main_Atrium" data-name="Main Atrium">
-  <circle class="cls-3" cx="177.69" cy="329.67" r="107.94" />
+  <circle class="cls-3 Main_Atrium" cx="177.69" cy="329.67" r="107.94" />
 </g>
 <g id="N311">
   <path
-    class="cls-3"
+    class="cls-3 N311"
     d="M664.36,480.61c9.23,10.45,24.09,27.12,50.14,38.75a155.29,155.29,0,0,0,84.61,11.7q-7.23-37.5-14.45-75a54.2,54.2,0,0,1-61.91-22C702.57,449.66,684.54,465,664.36,480.61Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N318">
   <path
-    class="cls-3"
+    class="cls-3 N318"
     d="M836.94,568.89l-15.82-45.4-18.57,4.82q-6.88-35.08-13.76-70.17a98.17,98.17,0,0,0,37.83-16.51l47.47,57.78-8.94,6.2,28.2,33a95.71,95.71,0,0,1-56.41,30.27Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N317">
   <path
-    class="cls-3"
+    class="cls-3 N317"
     d="M764.94,536.1q-2.3,20.64-4.58,41.28a113.54,113.54,0,0,0,71.54-8.26q-6.2-21.33-12.38-42.65a87.77,87.77,0,0,1-54.58,9.63Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N318a">
   <path
-    class="cls-3"
+    class="cls-3 N318a"
     d="M872.25,505.26c4.74,6.42,11.78,13.42,16.51,19.84l32.11-22.48q-9.18-11.91-18.35-23.84C893.2,487.8,881.58,496.24,872.25,505.26Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N319">
   <path
-    class="cls-3"
+    class="cls-3 N319"
     d="M905.73,475.57l17,23.84,7.34-3.66,7.79,10.08,10.55-9.63q-14-17.88-28-35.77Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N320">
   <path
-    class="cls-3"
+    class="cls-3 N320"
     d="M947.92,456.31,967.64,482l65.12-52.74L1002,392.56l-43.11,36.23L972.23,449l-9.17,6-5-5.5Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N325">
   <path
-    class="cls-3"
+    class="cls-3 N325"
     d="M1041,267.59,1074.5,313l5.5-4.12,9.17,12.84a647,647,0,0,1,50.91-40.36c15.54-11.1,30.62-20.83,44.94-29.35q-17-34.86-33.94-69.71A679.46,679.46,0,0,0,1041,267.59Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N328">
   <path
-    class="cls-3"
+    class="cls-3 N328"
     d="M1154.52,180.46q16.29,33.82,32.56,67.64a184.41,184.41,0,0,1,60.77-16.74l-4.59-59.16,3-.91q2.52,29.93,5,59.84a121.64,121.64,0,0,1,60.53,6.42l5.73-18.34-6.65-3.44q7-28,14-56a244.61,244.61,0,0,0-170.37,20.64Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N331">
   <path
-    class="cls-3"
+    class="cls-3 N331"
     d="M1363.87,233.84q17.39-35.44,34.78-70.88l84,39.63q-5.31,11.29-10.61,22.57l14.15,6.66q-12.24,24.87-24.46,49.76Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N334">
   <path
-    class="cls-3"
+    class="cls-3 N334"
     d="M1466.9,287.08l25.38-52.58,30.57,14.06-26,52.28Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N336">
   <path
-    class="cls-4"
+    class="cls-4 N336"
     d="M1493.2,318l41,19.87q16.65-33.78,33.32-67.56l-40.66-20.49Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N329">
   <path
-    class="cls-4"
+    class="cls-4 N329"
     d="M1335.13,288.31c13.35,6.52,28.23,14.26,41.58,20.79,5.81-11.62,11.62-25.07,17.43-36.69-13.66-7.34-27.31-12.84-41-20.18C1347.67,264.66,1340.64,275.87,1335.13,288.31Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N332">
   <path
-    class="cls-4"
+    class="cls-4 N332"
     d="M1381.15,310.78c8.46,4.28,18.14,10.39,26.6,14.67l19.56-36.38-30-14.37C1392.36,287.34,1386.14,298.14,1381.15,310.78Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N339">
   <path
-    class="cls-3"
+    class="cls-3 N339"
     d="M1205,469a80,80,0,0,1-16.81-40.05,82.82,82.82,0,0,1,7-43.41,85.15,85.15,0,0,1,32.41-36.69l28.74,16.2Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N340">
   <path
-    class="cls-3"
+    class="cls-3 N340"
     d="M1236.08,417q11.76-25.83,23.54-51.67l101.2,49.53a67,67,0,0,1-14.68,57.17Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N341">
   <path
-    class="cls-3"
+    class="cls-3 N341"
     d="M1208.25,472.05a59.5,59.5,0,0,0,28.13,19l-7,11.92a80,80,0,0,0,21.71,8.87c35.66,8.67,65-11,69.09-13.76a88.75,88.75,0,0,0,22.93-23.24l-110.06-55Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N346">
   <path
-    class="cls-4"
+    class="cls-4 N346"
     d="M840.15,659.46Q838.1,642.73,836,626l78-11q2.06,17.88,4.12,35.77Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N345">
   <path
-    class="cls-3"
+    class="cls-3 N345"
     d="M820,639.75l10.55-2.3,6.42,28,88-13.3q-1.83-16.29-3.67-32.56h16.06l14.67,99.51L836,738.8Q828,689.27,820,639.75Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="N344">
   <path
-    class="cls-3"
+    class="cls-3 N344"
     d="M940.43,618.5l20.49-2.45q2,13.76,4,27.52l110.68-15.9q6,37.46,11.92,74.9L957.86,723.36Z"
     transform="translate(-192 -147.9)"
   />
   <path
-    class="cls-3"
+    class="cls-3 N344"
     d="M1078.17,609q6.87,45.4,13.75,90.8l43.11-7.34,41.73-75.21-7.79-45.4-74.29,11.93.45,10.54-12.84.92q1.38,6.42,2.75,12.84Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="bathroom_1" data-name="bathroom 1">
   <path
-    class="cls-5"
+    class="cls-5 bathroom_1"
     d="M624,508.43l26.29,26.3q14.84-17.28,29.66-34.55L659.46,480Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="bathroom_2" data-name="bathroom 2">
   <path
-    class="cls-5"
+    class="cls-5 bathroom_2"
     d="M725.81,572.48l30,7.34,4.59-44.33-24.16-4.28Q731,551.84,725.81,572.48Z"
     transform="translate(-192 -147.9)"
   />
@@ -427,63 +387,63 @@ const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1563.59 604.1"
 </g>
 <g id="Accessible_toilet_1" data-name="Accessible toilet 1">
   <path
-    class="cls-6"
+    class="cls-6 accessible_toilet_1"
     d="M673.68,525.1l21.1,14.21L703,519.13l-17.88-11.92Q679.41,516.15,673.68,525.1Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="Accessible_toilet_2" data-name="Accessible toilet 2">
   <path
-    class="cls-6"
+    class="cls-6 accessible_toilet_2"
     d="M1207.49,272.87l5.5,16.05,23.39-6.88q-2.3-9.18-4.58-18.35Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="Accessible_toilet_3" data-name="Accessible toilet 3">
   <path
-    class="cls-6"
+    class="cls-6 accessible_toilet_3"
     d="M1417.07,317.35l11,6.42q6.18-10.32,12.38-20.64l-14.22-4.58Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="lift_1" data-name="lift 1">
   <path
-    class="cls-7"
+    class="cls-7 lift_1"
     d="M516.38,546.19l11,16.51,21.09-16.05c-4-4.75-8.23-11.11-12-15.36C530.18,535.26,522.65,542.22,516.38,546.19Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="lift_2" data-name="lift 2">
   <path
-    class="cls-7"
+    class="cls-7 lift_2"
     d="M583.8,442.09l15.28,18.65,16.21-12.23q-7.35-9-14.68-18Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="lift_3" data-name="lift 3">
   <path
-    class="cls-7"
+    class="cls-7 lift_3"
     d="M1046.37,387.36l7.34,11.47,17.58-12.23L1063,375.44Z"
     transform="translate(-192 -147.9)"
   />
 </g>
 <g id="lift_4" data-name="lift 4">
   <path
-    class="cls-7"
+    class="cls-7 lift_4"
     d="M1306.24,349.61l4.59-11,19.87,8.86-4.89,11.62Z"
     transform="translate(-192 -147.9)"
   />
 </g>
-<g id="lft_5" data-name="lft 5">
+<g id="lift_5" data-name="lft 5">
   <path
-    class="cls-7"
+    class="cls-7 lift_5"
     d="M1432.05,327.52q7,4.28,14.06,8.56l9.18-21.4-14.37-7.34Q1436.48,317.42,1432.05,327.52Z"
     transform="translate(-192 -147.9)"
   />
 </g>
-<g id="luft_6" data-name="luft 6">
+<g id="lift_6" data-name="lift 6">
   <path
-    class="cls-7"
+    class="cls-7 lift_6"
     d="M969.86,610.24l1.53,12.84,19-3.36V606.88Z"
     transform="translate(-192 -147.9)"
   />
@@ -494,7 +454,9 @@ const app = document.querySelector("#app");
 const scene = setupScene(app);
 const { object, highlight } = renderSVG(svg);
 
-highlight({x: 777.86, y: 462.34000000000003});
+highlight("N309");
 
 scene.add(object);
+
+export default highlight;
 
